f6711f1da5066e383a961acb17bebcc656da25e3
Ricardo Martins <ricardo.parallax@gmail.com>
2025-06-10 15:42:34 -0300

    Update FormatPhoneNumberPlugin.cs

--- FormatPhoneNumberPlugin.cs
+++ FormatPhoneNumberPlugin.cs
@@ -1,49 +1,66 @@
 using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
 using System;
-using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
 
-public class FormatPhoneNumberPlugin : IPlugin
+public class PreCreateRegistroVisitaPlugin : IPlugin
 {
     public void Execute(IServiceProvider serviceProvider)
     {
         var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
         var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
         var service = serviceFactory.CreateOrganizationService(context.UserId);
+        var tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
 
-        // Verifica se está no PreOperation e nas mensagens desejadas
-        if (context.Stage != 20 || (context.MessageName != "Create" && context.MessageName != "Update"))
+        if (context.MessageName != "Create" || context.Stage != 20)
             return;
 
-        if (!context.InputParameters.Contains("Target") || !(context.InputParameters["Target"] is Entity entity))
+        if (!(context.InputParameters["Target"] is Entity entity) || entity.LogicalName != "vc4_registrodevisita")
             return;
 
-        if (entity.LogicalName != "contact")
+        string valores = "";
+        int tipoRegistro = entity.GetAttributeValue<OptionSetValue>("vc4_tipoderegistro")?.Value ?? -1;
+
+        string fieldToUse = tipoRegistro == 947750000 ? "vc4_objetivodavisita" : "vc4_demandarecebida";
+
+        if (!entity.Attributes.Contains(fieldToUse))
             return;
 
-        if (!entity.Attributes.Contains("telephone1") || entity["telephone1"] == null)
+        var optionSetValues = entity.GetAttributeValue<OptionSetValueCollection>(fieldToUse);
+
+        if (optionSetValues == null || !optionSetValues.Any())
             return;
 
-        var telefoneOriginal = entity["telephone1"].ToString();
-        string telefoneNumerico = Regex.Replace(telefoneOriginal, "[^0-9]", "");
+        var labels = GetOptionSetLabels(service, "vc4_registrodevisita", fieldToUse, optionSetValues);
+        valores = string.Join(", ", labels);
 
-        // Limita aos últimos 11 dígitos se houver mais
-        if (telefoneNumerico.Length > 11)
-            telefoneNumerico = telefoneNumerico.Substring(telefoneNumerico.Length - 11);
+        entity["vc4_name"] = valores;
+    }
 
-        if (telefoneNumerico.Length < 10)
-            throw new InvalidPluginExecutionException("O número de telefone é muito curto. Insira pelo menos 10 dígitos.");
+    private List<string> GetOptionSetLabels(IOrganizationService service, string entityName, string fieldName, OptionSetValueCollection values)
+    {
+        var labels = new List<string>();
 
-        string telefoneFormatado;
-
-        if (telefoneNumerico.Length == 11)
+        var request = new RetrieveAttributeRequest
         {
-            telefoneFormatado = $"({telefoneNumerico.Substring(0, 2)}) {telefoneNumerico.Substring(2, 5)}-{telefoneNumerico.Substring(7, 4)}";
-        }
-        else // 10 dígitos
+            EntityLogicalName = entityName,
+            LogicalName = fieldName,
+            RetrieveAsIfPublished = true
+        };
+
+        var response = (RetrieveAttributeResponse)service.Execute(request);
+        var metadata = (MultiSelectPicklistAttributeMetadata)response.AttributeMetadata;
+
+        foreach (var value in values)
         {
-            telefoneFormatado = $"({telefoneNumerico.Substring(0, 2)}) {telefoneNumerico.Substring(2, 4)}-{telefoneNumerico.Substring(6, 4)}";
+            var option = metadata.OptionSet.Options.FirstOrDefault(o => o.Value == value.Value);
+            if (option != null)
+                labels.Add(option.Label.UserLocalizedLabel.Label);
         }
 
-        entity["telephone1"] = telefoneFormatado;
+        return labels;
     }
 }
